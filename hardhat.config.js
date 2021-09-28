4a5088ffe07345eb169d6274b0c94cd76e54d7ba
ernestognw <ernestognw@gmail.com>
2021-09-28 12:50:48 -0500

    class(4): Private key management

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,11 +1,15 @@
 require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
+
+const projectId = process.env.INFURA_PROJECT_ID;
+const privateKey = process.env.DEPLOYER_SIGNER_PRIVATE_KEY;
 
 module.exports = {
   solidity: "0.8.4",
   networks: {
     rinkeby: {
-      url: "https://rinkeby.infura.io/v3/<<PASTE YOUR INFURA PROJECT ID (DANGEROUS)>>",
-      accounts: ["<<PASTE YOUR PRIVATE KEY HERE (DANGEROUS)>>"],
+      url: `https://rinkeby.infura.io/v3/${projectId}`,
+      accounts: [privateKey],
     },
   },
 };
